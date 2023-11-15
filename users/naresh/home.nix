072add2fab4a349940bdf064ad58cf26de8062dd
npnpatidar <nixos@whatislef.anonaddy.com>
2023-11-15 22:42:22 +0530

    moves alias from config to home

--- users/naresh/home.nix
+++ users/naresh/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs,  ... }:
+{ config, pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -88,6 +88,39 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.bash.enable = true;
+  programs.bash.shellAliases = {
+    c = "clear";
+    sn = "sudo nano";
+    htop = "btop";
+    h = "history | grep $1";
+    rebash = " source ~/.bashrc";
+    e = "exit";
+    ti = "tgpt -i";
+    mlc = "cd /home/naresh/Data/Sync_M_L_C && ls";
+    ml = "cd /home/naresh/Data/Sync_M_L && ls";
+    lc = "cd /home/naresh/Data/Sync_L_C && ls";
+    nl = "cd /home/naresh/Data/Sync_N_Laptop && ls";
+    mydoc = "cd /home/naresh/Data/Sync_M_L/Documents/MyDoc/ && ls";
+    docs = "cd /home/naresh/Data/Sync_M_L/Documents/ && ls";
+    ch = "function _curlcheat() { curl cheat.sh/'$1' } _curlcheat";
+    net = "sudo nethogs";
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    "...." = "cd ../../..";
+    zd = "zoxide add '$(pwd)'";
+    doc_backup = "rclone sync /home/naresh/Data/Sync_M_L/Documents/ /home/naresh/.local/share/Cryptomator/mnt/EncryptedDocuments/   --verbose ";
+    snc = "nano ~/.dotfiles/system/configuration.nix";
+    snm = "nano ~/.dotfiles/users/naresh/home.nix";
+    snr = "sudo nixos-rebuild";
+    nos = "nix --extra-experimental-features 'nix-command flakes' search nixpkgs";
+    note = "notepadqq";
+    cnd = "code ~/.dotfiles";
+    as = "~/.dotfiles/apply-system.sh";
+    au = "~/.dotfiles/apply-users.sh";
+  };
+
 
   # programs.plasma = {
   #   enable = true;
