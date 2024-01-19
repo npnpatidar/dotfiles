58f7e463563d2c361fbcc04083966386a6913a6e
npnpatidar <7de6dkm1@duck.com>
2024-01-19 08:50:33 +0530

    syncthing tray, flake update, helix removed

--- modules/home-manager/applications.nix
+++ modules/home-manager/applications.nix
@@ -55,6 +55,7 @@
     gnome.gnome-boxes
     newsflash
     # bitwarden
+    inkscape-with-extensions
 
 
     (nerdfonts.override { fonts = [ "FiraCode" "FiraMono" "DroidSansMono" "Hack" "Hasklig" "Meslo" "UbuntuMono" ]; })
@@ -163,7 +164,6 @@
     ttyper
     gpg-tui
     termdbms
-    helix
     bitwarden-cli
     zathura
     lazydocker
--- modules/home-manager/librewolf.nix
+++ modules/home-manager/librewolf.nix
@@ -21,7 +21,7 @@ let
         (extension "xdm-browser-monitor-v8" "xdm-v8-browser-helper@subhra74.github.io")
         (extension "floccus" "floccus@handmadeideas.org")
         (extension "skip-redirect" "skipredirect@sblask")
-        (extension "darkreader" "addon@darkreader.org")
+        # (extension "darkreader" "addon@darkreader.org")
         (extension "absolute-enable-right-click" "{9350bc42-47fb-4598-ae0f-825e3dd9ceba}")
         (extension "bypass-paywalls-clean-d" "magnolia_limited_permissions_d@12.34")
         (extension "i-dont-care-about-cookies" "jid1-KKzOGWgsW3Ao4Q@jetpack")
@@ -29,6 +29,7 @@ let
         (extension "autofill-quantum" "{143f479b-4cb2-4d8c-8c31-ae8653bc6054}")
         (extension "imagus" "{00000f2a-7cde-4f20-83ed-434fcb420d71}")
         (extension "omnivore" "save-extension@omnivore.app")
+        (extension "midnight-lizard-quantum" "{8fbc7259-8015-4172-9af1-20e1edfbbd3a}")
       ];
     AppAutoUpdate = false;
   };
