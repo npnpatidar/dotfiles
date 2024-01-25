37d88bd6c232d5d7dbbc2b1e223ef27016dd735a
npnpatidar <7de6dkm1@duck.com>
2024-01-25 12:01:48 +0530

    thorium upgraded

--- pkgs/thorium.nix
+++ pkgs/thorium.nix
@@ -2,17 +2,17 @@
 let
   pname = "thorium-browser";
   sname = "thorium";
-  version = "117.0.5938.157";
+  version = "120.0.6099.235";
   name = "${pname}-${version}";
   rev = "1"; # Update this when you make changes to the derivation
   src = pkgs.fetchurl {
     url = "https://github.com/Alex313031/thorium/releases/download/M${version}/Thorium_Browser_${version}_x64.AppImage";
-    sha256 = "sha256-dlfClBbwSkQg4stKZdSgNg3EFsWksoI21cxRG5SMrOM=";
+    sha256 = "sha256-HVqC0uk5Ia1xolLvCwDl42VXAUwkikqRasNdLOe8SUs=";
   };
   appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
 in
 pkgs.appimageTools.wrapType2 {
-  inherit name src ;
+  inherit name src;
   extraInstallCommands = ''
     mv $out/bin/${name} $out/bin/${sname}
     install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
