e42b4763ffec2c71789b4d84af05a4b79f1c7e7d
npnpatidar <7de6dkm1@duck.com>
2024-02-21 13:41:28 +0530

    tiliing assistant added

--- modules/nixos/desktop_config/gnome_config.nix
+++ modules/nixos/desktop_config/gnome_config.nix
@@ -55,7 +55,11 @@
     syncthing-indicator
     # prime-helper
     # gpu-profile-selector
-
+    lock-keys
+    tiling-assistant # 'tiling-assistant@leleat-on-github'
+    # forge    # "forge@jmmaranan.com"
+    pop-shell #pop-shell@system76.com
+    gtk4-desktop-icons-ng-ding #'gtk4-ding@smedius.gitlab.com
   ]);
 
 
