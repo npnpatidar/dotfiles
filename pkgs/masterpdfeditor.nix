1748ac58ae80fe42593942e7e84beceea49904ab
npnpatidar <7de6dkm1@duck.com>
2024-03-30 23:51:18 +0530

    masterpdf code updated

--- pkgs/masterpdfeditor.nix
+++ pkgs/masterpdfeditor.nix
@@ -8,6 +8,7 @@ pkgs.stdenv.mkDerivation rec {
   src = pkgs.fetchurl {
     url = "https://code-industry.net/public/master-pdf-editor-${version}-qt5.x86_64.tar.gz";
     sha256 = "sha256-c5DYS0PQemZ8Sql2KjnuMspCLDJzU95rsbuIdoxWDM0="; #5.9.35
+    # sha256 = "sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=";
     # sha256 = "sha256-CbrhhQJ0iiXz8hUJEi+/xb2ZGbunuPuIIgmCRgJhNVU="; # 5.9.82
     # sha256 = "sha256-CkNg27w6XQ2eAjhA4h8z2RNDM3B2leN9Q3J6TUXPSGs="; # 5.9.20
   };
