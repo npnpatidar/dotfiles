6e8876741e7ff557c73166bf83256aa80f70b2aa
npnpatidar <7de6dkm1@duck.com>
2024-04-05 22:42:28 +0530

    rclone google drive shared - working great

--- modules/home-manager/rclone.nix
+++ modules/home-manager/rclone.nix
@@ -7,7 +7,20 @@ let
         Environment = "PATH=/run/wrappers/bin";
 
         ExecStart = ''
-          ${pkgs.rclone}/bin/rclone bisync ${environment}:/ ${config.home.homeDirectory}/Data/${environment}  --filter-from   ${config.home.homeDirectory}/.config/rclone/${environment}.txt  --force  --drive-import-formats=docx --verbose --create-empty-src-dirs  --resilient --check-first  --recover
+          ${pkgs.rclone}/bin/rclone bisync ${environment}:/ ${config.home.homeDirectory}/Data/${environment}/Drive  --filter-from   ${config.home.homeDirectory}/.config/rclone/${environment}.txt  --force  --drive-import-formats=docx --verbose --create-empty-src-dirs  --resilient --check-first  --recover
+
+        '';
+      };
+    };
+
+  mkRcloneSharedService = environment:
+    lib.nameValuePair "rclone-shared-${environment}" {
+      Service = {
+        Type = "simple";
+        Environment = "PATH=/run/wrappers/bin";
+
+        ExecStart = ''
+          ${pkgs.rclone}/bin/rclone bisync ${environment}:/ ${config.home.homeDirectory}/Data/${environment}/Shared  --filter-from   ${config.home.homeDirectory}/.config/rclone/${environment}-shared.txt  --drive-import-formats=docx --verbose --create-empty-src-dirs  --resilient --check-first  --recover --drive-shared-with-me
 
         '';
       };
@@ -25,19 +38,36 @@ let
       };
       Install = { WantedBy = [ "timers.target" ]; };
     };
+
+  mkSharedSyncTimer = environment:
+    lib.nameValuePair "rclone-shared-${environment}" {
+      Timer = {
+        # 5 minutes after boot
+        OnBootSec = "2m";
+        # 5 minutes after last finished
+        OnUnitInactiveSec = "5m";
+        # run once when the timer is started
+        Unit = "rclone-shared-${environment}.service";
+      };
+      Install = { WantedBy = [ "timers.target" ]; };
+    };
 in
 {
   systemd.user.services = builtins.listToAttrs
     (map mkRcloneService [
       "naresh.alternate"
       "npnpatidar"
-    ]) // { };
+    ]) // builtins.listToAttrs (map mkRcloneSharedService [
+    "npnpatidar"
+  ]) // { };
 
   systemd.user.timers = builtins.listToAttrs
     (map mkSyncTimer [
       "naresh.alternate"
       "npnpatidar"
-    ]) // { };
+    ]) // builtins.listToAttrs (map mkSharedSyncTimer [
+    "npnpatidar"
+  ]) // { };
 
 
   # rclone bisync naresh.alternate: ~/Data/naresh.alternate --resync --filter-from ~/.config/rclone/naresh.alternate.txt 
@@ -53,6 +83,13 @@ in
   home.file.".config/rclone/npnpatidar.txt".text = ''
     - rcloneCrypt/**
     - EncryptedDocuments/**
+  '';
+  home.file.".config/rclone/npnpatidar-shared.txt".text = ''
+    + BOOKS/Naresh\ books\ content/Edited\ By\ Naresh/**
+    - BOOKS/**
+    - CLASSIFICATION/**
+    - ratta\ course/**
+    - Naresh\ Data\ purification/**  
   '';
 }
 
