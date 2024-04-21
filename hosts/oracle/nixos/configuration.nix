572412026c34bb34c9d689da3e5e5c990496ec06
npnpatidar <7de6dkm1@duck.com>
2024-04-21 12:58:49 +0530

    new user removed

--- hosts/oracle/nixos/configuration.nix
+++ hosts/oracle/nixos/configuration.nix
@@ -39,16 +39,16 @@
   time.timeZone = "Asia/Kolkata";
 
 
-  users.users.naresh = {
-    isNormalUser = true;
-    initialPassword = "naresh";
-    description = "naresh";
-    extraGroups = [ "networkmanager" "wheel" "kvm" "input" "disk" "libvirtd" "usbmux" ];
-    createHome = true;
-    home = "/home/naresh";
-    shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [ ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8pK+/SUI3dPB1tQ0nF4Gp9BKKGMHnJ1bBSiYJX2sCHgbfOmDKAlAnuRTP6Zhp6BTZ5LwNC/4pI76bnpmo8YjjGNGkPlMHfOHrn8rm2Hhyx7RVHyMLGKYQdNtzBcfPgDUqrXPM3cdCMya15BnavXE4fOYUoGgIvOolTveWfngHRjQNptTlfpQoIjMRIvIfhu+xLiikJVm4EbgzEVu6U8OdGuV8eq33GYc+HORqKRq+jILIT5V3q4OTcCbORbStt4Zq4WumoVWXuM3abmzpA0nCAbZM8ArWQ8UujOM490hyQVGqfZae8FS1ADGAyEybrHMIMxT0IysZ7xW+tnaljIpt ssh-key-2024-04-15'' ];
-  };
+  # users.users.naresh = {
+  #   isNormalUser = true;
+  #   initialPassword = "naresh";
+  #   description = "naresh";
+  #   extraGroups = [ "networkmanager" "wheel" "kvm" "input" "disk" "libvirtd" "usbmux" ];
+  #   createHome = true;
+  #   home = "/home/naresh";
+  #   shell = pkgs.zsh;
+  #   openssh.authorizedKeys.keys = [ ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8pK+/SUI3dPB1tQ0nF4Gp9BKKGMHnJ1bBSiYJX2sCHgbfOmDKAlAnuRTP6Zhp6BTZ5LwNC/4pI76bnpmo8YjjGNGkPlMHfOHrn8rm2Hhyx7RVHyMLGKYQdNtzBcfPgDUqrXPM3cdCMya15BnavXE4fOYUoGgIvOolTveWfngHRjQNptTlfpQoIjMRIvIfhu+xLiikJVm4EbgzEVu6U8OdGuV8eq33GYc+HORqKRq+jILIT5V3q4OTcCbORbStt4Zq4WumoVWXuM3abmzpA0nCAbZM8ArWQ8UujOM490hyQVGqfZae8FS1ADGAyEybrHMIMxT0IysZ7xW+tnaljIpt ssh-key-2024-04-15'' ];
+  # };
 
 
   services.ollama = {
@@ -193,7 +193,7 @@
       address = "127.0.0.1";
       port = 2342;
       passwordFile = "/etc/nextcloud-admin-pass";
-      originalsPath = "/home/naresh/photoprism";
+      originalsPath = "/root/photoprism";
     };
 
 }
