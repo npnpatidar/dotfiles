ccd642a5eb434d30ea9ff6fc67a6d78353b0d139
npnpatidar <7de6dkm1@duck.com>
2024-05-04 01:50:30 +0530

    openwebui in local

--- hosts/aspire7/nixos/configuration.nix
+++ hosts/aspire7/nixos/configuration.nix
@@ -41,6 +41,7 @@
       ../../../modules/nixos/fonts.nix
       # ../../../modules/nixos/servarr.nix
       ./hardware-configuration.nix
+      # ../../../modules/nixos/ollama.nix
     ];
 
 
@@ -100,10 +101,33 @@
 
 
   services.ollama = {
-
     enable = true;
     acceleration = "cuda";
-    listenAddress = "127.0.0.1:11434";
-
   };
+
+  virtualisation = {
+    oci-containers = {
+      containers = {
+        open-webui = {
+          image = "ghcr.io/open-webui/open-webui:main";
+          autoStart = true;
+          ports = [
+            "127.0.0.1:8080:8080"
+          ];
+          volumes = [
+            "open-webui:/app/backend/data"
+          ];
+          environment = {
+            OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+            ANONYMIZED_TELEMETRY = "False";
+          };
+          extraOptions = [
+            "--network=host"
+            "--add-host=host.containers.internal:host-gateway"
+          ];
+        };
+      };
+    };
+  };
+
 }
