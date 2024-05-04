eae1227715f714cb49389120f2a4975b1fd65e9f
npnpatidar <7de6dkm1@duck.com>
2024-05-05 01:22:53 +0530

    homepage dashboard setup proper

--- hosts/oracle/nixos/configuration.nix
+++ hosts/oracle/nixos/configuration.nix
@@ -111,45 +111,148 @@
   services.homepage-dashboard = {
     enable = true;
     listenPort = 8888;
+    settings = {
+      # base = "https://dashboard.internal.flake.sh/";
+      title = "Naresh";
+      language = "en";
+      headerStyle = "boxedWidgets";
+      disableCollape = true;
+      favicon = "https://em-content.zobj.net/source/twitter/376/cloud_2601-fe0f.png";
+      cardBlur = "md";
+      theme = "dark";
+      color = "gray";
+      fiveColumns = true;
+      statusStyle = "dot";
+      hideVersion = true;
+      background = {
+        # image = "https://media.githubusercontent.com/media/lunik1/nix-wallpaper/assets/nord-night.png";
+        # image = "https://github.com/NixOS/nixos-artwork/blob/f38303238fb03b89e29b6c8c2fd32e59df59a2d5/wallpapers/nix-wallpaper-simple-blue.png";
+        # image = ../../../modules/home-manager/stylix/nix.png;
+        image = "https://github.com/NixOS/nixos-artwork/blob/master/wallpapers/nix-wallpaper-gear.png?raw=true";
+        # brightness = 50;
+        # blur = "sm";
+        # saturate = 50;
+        opacity = 50;
+      };
+      providers = {
+        # openweathermap = "{{HOMEPAGE_VAR_OPENWEATHERMAP}}";
+      };
+      layout = [
+        {
+          "Services" = {
+            style = "row";
+            columns = 5;
+          };
+        }
+        {
+          "Nix" = {
+            style = "row";
+            columns = 5;
+          };
+        }
+        {
+          "Work" = {
+            style = "row";
+            columns = 5;
+          };
+        }
+      ];
+    };
     services =
       [
         {
           "Services" = [
             {
               "Nextcloud" = {
-                description = "Nextcloud";
+                icon = "nextcloud";
                 href = "http://nextcloud.naresh.world";
+                # widget = {
+                #   type = "nextcloud";
+                #   url = "http://nextcloud.naresh.world";
+                #   username = "naresh";
+                #   password = "";
+                # };
               };
+            }
+            {
               "Freshrss" = {
-                description = "Freshrss";
+                icon = "freshrss";
                 href = "http://freshrss.naresh.world";
               };
+            }
+            {
               "Ollama" = {
-                description = "Ollama";
+                icon = "https://ollama.com/public/ollama.png";
                 href = "http://ollama.naresh.world";
               };
+            }
+            {
               "Chat" = {
-                description = "Chat";
+                icon = "open-webui";
                 href = "http://chat.naresh.world";
               };
+            }
+            {
+              "Local Chat" = {
+                icon = "open-webui";
+                href = "http://localhost:8080";
+              };
+            }
+            {
+              "News" = {
+                icon = "https://newsboat.org/logo.svg";
+                href = "http://news.naresh.world";
+              };
+            }
+            {
               "VaultWarden" = {
-                description = "VaultWarden";
+                icon = "vaultwarden";
                 href = "http://vaultwarden.naresh.world";
               };
             }
+            {
+              "Paperless" = {
+                icon = "paperless-ngx";
+                href = "http://paperless.naresh.world";
+              };
+            }
+            {
+              "Files" = {
+                icon = "files";
+                href = "http://files.naresh.world";
+              };
+            }
+            {
+              "Code" = {
+                icon = "vscode";
+                href = "http://code.naresh.world";
+              };
+            }
+            # {
+            #   "NextDNS" = {
+            #     icon = "nextdns";
+            #     widget = {
+            #       type = "nextdns";
+            #       profileid = "iec4ca1";
+            #       key = "";
+            #     };
+            #   };
+            # }
           ];
         }
       ];
     bookmarks = [
       {
-        Developer = [
+        Nix = [
           {
-            Nix = [
+            Nixos = [
               {
                 abbr = "Nix";
                 href = "https://search.nixos.org/options?channel=unstable";
               }
             ];
+          }
+          {
             Github = [
               {
                 abbr = "Git";
@@ -177,6 +280,7 @@
         resources = {
           cpu = true;
           memory = true;
+          cputemp = true;
           disk = "/";
           uptime = true;
           units = "metric";
@@ -187,8 +291,9 @@
       }
       {
         search = {
-          provider = "duckduckgo";
+          provider = [ "duckduckgo" "brave" "google" ];
           target = "_blank";
+          # showSearchSuggestions = true;
         };
       }
     ];
