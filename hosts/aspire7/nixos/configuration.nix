1e04a1068b099be12e9976b2d7d74392acf157be
npnpatidar <7de6dkm1@duck.com>
2024-05-08 18:22:40 +0530

    cleaning up

--- hosts/aspire7/nixos/configuration.nix
+++ hosts/aspire7/nixos/configuration.nix
@@ -1,9 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, lib, pkgs, inputs, ... }:
-
 {
 
   nixpkgs.config.allowUnfree = true;
@@ -70,12 +65,12 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.naresh = {
     isNormalUser = true;
-    initialPassword = "naresh";
     description = "naresh";
     extraGroups = [ "networkmanager" "wheel" "kvm" "input" "disk" "libvirtd" "usbmux" ];
     createHome = true;
     home = "/home/naresh";
     shell = pkgs.zsh;
+    hashedPasswordFile = config.age.secrets."hashedstandard".path;
   };
 
   environment.systemPackages = [ inputs.agenix.packages.x86_64-linux.default ];
--- hosts/oracle/nixos/configuration.nix
+++ hosts/oracle/nixos/configuration.nix
@@ -12,8 +12,6 @@
   zramSwap.enable = true;
   networking.hostName = "alma";
   networking.domain = "";
-  # services.openssh.enable = true;
-  # services.openssh.settings.PasswordAuthentication = false;
   users.users.root.openssh.authorizedKeys.keys = [ ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8pK+/SUI3dPB1tQ0nF4Gp9BKKGMHnJ1bBSiYJX2sCHgbfOmDKAlAnuRTP6Zhp6BTZ5LwNC/4pI76bnpmo8YjjGNGkPlMHfOHrn8rm2Hhyx7RVHyMLGKYQdNtzBcfPgDUqrXPM3cdCMya15BnavXE4fOYUoGgIvOolTveWfngHRjQNptTlfpQoIjMRIvIfhu+xLiikJVm4EbgzEVu6U8OdGuV8eq33GYc+HORqKRq+jILIT5V3q4OTcCbORbStt4Zq4WumoVWXuM3abmzpA0nCAbZM8ArWQ8UujOM490hyQVGqfZae8FS1ADGAyEybrHMIMxT0IysZ7xW+tnaljIpt ssh-key-2024-04-15'' ];
   system.stateVersion = "23.11";
   nixpkgs.hostPlatform = "aarch64-linux";
@@ -66,9 +64,8 @@
       AllowUsers = [ "naresh" "git" ];
     };
     extraConfig = "MaxAuthTries 10";
-
-
   };
+
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud28;
@@ -120,11 +117,11 @@
     telemetryLevel = "off";
     withoutConnectionToken = true;
   };
+
   services.homepage-dashboard = {
     enable = true;
     listenPort = 8888;
     settings = {
-      # base = "https://dashboard.internal.flake.sh/";
       title = "Naresh";
       language = "en";
       headerStyle = "boxedWidgets";
