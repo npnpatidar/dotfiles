8316f217020d253b66814b04ad5f82602850bbd9
npnpatidar <7de6dkm1@duck.com>
2024-05-17 20:49:51 +0530

    everyting working except push to create

--- hosts/oracle/nixos/gitdaemon-server.nix
+++ hosts/oracle/nixos/gitdaemon-server.nix
@@ -1,20 +1,21 @@
 { pkgs
 , ...
 }: {
-
-
   services.gitea = {
     enable = true;
-    # user = "git";
     settings = {
       server = {
         SSH_PORT = 46587;
         HTTP_PORT = 5654;
-        # ROOT_URL = "https://test.naresh.world";
         DOMAIN = "git.naresh.world";
-        # ROOT_URL = "git.naresh.world";
-        # PROTOCOL = "https";
-        # httpAddress = "127.0.0.1";
+      };
+      repository = {
+        DEFAULT_PUSH_CREATE_PRIVATE = true;
+        ENABLE_PUSH_CREATE_USER = true;
+        DEFAULT_PRIVATE = true;
+      };
+      service = {
+        DISABLE_REGISTRATION = true;
       };
     };
   };
@@ -27,9 +28,6 @@
         proxyPass = "http://127.0.0.1:5654";
       };
     };
-    #
-
-
   };
 
   # services.gitea-actions-runner = {
@@ -55,15 +53,10 @@
   #
   users = {
     users.gitea = {
-      # home = "/var/lib/gitDaemon";
       createHome = false;
-      # homeMode = "755";
-      # isSystemUser = false;
       shell = "${pkgs.bash}/bin/bash";
       group = "gitea";
-      # packages = [ pkgs.git pkgs.gitea ];
       openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDy+wbDlo4wWVIsmK75dWh+9pIDJPcYa0swCtsLapo2esZtuJZJCgvH0yVXBJP6LNdpbV+7ARI2CQblomJcG03JHtMkcTiQIJU0XRKP33KlfMOSWrC+txbTi6YWqdrpljc9Tbk12dYW7/8bzs1FkoqYoVjqplyALGl7BLlWTphTAHUOaOJ1xX3V7y5EDKmCGfG7C0j9qIt8qq6DS9wg7kQuEKPp1MPjL1iyJqVfP/4KLwn/ATm7RZis7RgQS4W7TyIoSok+hrc7Sbg1rBtua5T/Cb3tgOHM1R3fCwTnJ5QAvCyw9yLxbsz5p2Kd+fTnwHVYopMFHYFprCaXyRstRVdD3+ImbpkH5B0Bxy64yHhymH31mBglqEu9eR8iG3DWy4oMGicFpdKbiXL+O26qsXBmlg+f/oEKTTE0YZVWy8Q226hIE69Wqn0gCHpdKiM9za1l94W9QYzwRLaZnmy1YvPFTjUZCiu59ZuadeTvx4RAPOemhhe4DfJ3+rpZk2ynsg8= gitserver@whatisleft.anonaddy.com" ];
     };
-    groups.gitea = { };
   };
 }
