b6157a3118b4fa9d7134d67cc9ded1354958cbbb
npnpatidar <7de6dkm1@duck.com>
2024-05-30 16:20:42 +0530

    Sahitya instead of Lohit font

--- hosts/aspire7/nixos/fonts.nix
+++ hosts/aspire7/nixos/fonts.nix
@@ -3,20 +3,26 @@
 
 
   fonts = {
-    enableDefaultPackages = true;
+    enableDefaultPackages = false;
     packages = with pkgs; [
       # nerdfonts
-      (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
-      lohit-fonts.devanagari
+      (import ../../../pkgs/sahityaFont.nix { inherit pkgs; })
+      (pkgs.nerdfonts.override {
+        fonts = [ "FiraCode" "NerdFontsSymbolsOnly" ];
+      })
+      # lohit-fonts.devanagari
+      # (pkgs.noto-fonts.override {
+      #   variants = [ "Noto Sans Symbols" "Noto Sans Symbols 2" "Noto Sans Math" "Noto Sans Devanagari" "Noto Serif Devanagari" ];
+      # })
       # noto-fonts
     ];
 
     fontconfig = {
       enable = true;
       defaultFonts = {
-        serif = [ "FiraCode Nerd Font" "Lohit Devanagari" ];
-        sansSerif = [ "FiraCode Nerd Font" "Lohit Devanagari" ];
-        monospace = [ "FiraCode Nerd Font Mono" "Lohit Devanagari" ];
+        serif = [ "FiraCode Nerd Font" "Sahitya" ];
+        sansSerif = [ "FiraCode Nerd Font" "Sahitya" ];
+        monospace = [ "FiraCode Nerd Font Mono" "Sahitya" ];
       };
     };
   };
