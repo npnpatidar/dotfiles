2138bef0ebe5b0fc81ba774c89bf73c8eff3d07a
npnpatidar <7de6dkm1@duck.com>
2024-07-01 19:43:36 +0530

    n8n manual install

--- hosts/oracle/nixos/n8n.nix
+++ hosts/oracle/nixos/n8n.nix
@@ -1,6 +1,72 @@
-{ config, ... }: {
+{ config, pkgs, ... }:
+let
+  cfg = config.services.n8n;
+  format = pkgs.formats.json { };
+  configFile = format.generate "n8n.json" cfg.settings;
+in
+{
   nixpkgs.config.allowUnfree = true;
-  services.n8n.enable = true;
+  # services.n8n.enable = true;
+
+
+  systemd.services.n8n = {
+    description = "N8N service";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    environment = {
+      # This folder must be writeable as the application is storing
+      # its data in it, so the StateDirectory is a good choice
+      N8N_USER_FOLDER = "/var/lib/n8n";
+      HOME = "/var/lib/n8n";
+      N8N_CONFIG_FILES = "${configFile}";
+      WEBHOOK_URL = "${cfg.webhookUrl}";
+
+      # Don't phone home
+      N8N_DIAGNOSTICS_ENABLED = "false";
+      N8N_VERSION_NOTIFICATIONS_ENABLED = "false";
+    };
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "/run/current-system/sw/bin/n8n";
+      Restart = "on-failure";
+      StateDirectory = "n8n";
+
+      # Basic Hardening
+      NoNewPrivileges = "yes";
+      PrivateTmp = "yes";
+      PrivateDevices = "yes";
+      DevicePolicy = "closed";
+      DynamicUser = "true";
+      ProtectSystem = "strict";
+      ProtectHome = "read-only";
+      ProtectControlGroups = "yes";
+      ProtectKernelModules = "yes";
+      ProtectKernelTunables = "yes";
+      RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6 AF_NETLINK";
+      RestrictNamespaces = "yes";
+      RestrictRealtime = "yes";
+      RestrictSUIDSGID = "yes";
+      MemoryDenyWriteExecute = "no"; # v8 JIT requires memory segments to be Writable-Executable.
+      LockPersonality = "yes";
+    };
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   services.nginx = {
     virtualHosts."n8n.${config.globals.domain_name}" = {
       enableACME = true;
