1955a3c6a97baa2fdb72853e381ac455ce555992
npnpatidar <7de6dkm1@duck.com>
2024-08-02 00:09:26 +0530

    n8n moved to podman - old is still there

--- hosts/oracle/nixos/n8n.nix
+++ hosts/oracle/nixos/n8n.nix
@@ -3,6 +3,7 @@ let
   cfg = config.services.n8n;
   format = pkgs.formats.json { };
   configFile = format.generate "n8n.json" cfg.settings;
+  datastorePath = "/var/lib/n8n";
 in
 {
   nixpkgs.config.allowUnfree = true;
@@ -54,30 +55,56 @@ in
       MemoryDenyWriteExecute = "no"; # v8 JIT requires memory segments to be Writable-Executable.
       LockPersonality = "yes";
     };
+    #
+
+
   };
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+  virtualisation = {
+    oci-containers.containers = {
+      n8n = {
+        image = "n8nio/n8n:next";
+        environment = {
+          # N8N_USER_FOLDER = "/var/lib/n8n";
+          # HOME = "/var/lib/n8n";
+          # N8N_CONFIG_FILES = "${configFile}";
+          WEBHOOK_URL = "https://n8n.naresh.world/";
+          GENERIC_TIMEZONE = "Asia/Kolkata";
+          NODE_FUNCTION_ALLOW_EXTERNAL = "*";
+          NODE_FUNCTION_ALLOW_BUILTIN = "*";
+          N8N_TEMPLATES_ENABLED = "true";
+          N8N_DIAGNOSTICS_ENABLED = "false";
+          N8N_VERSION_NOTIFICATIONS_ENABLED = "false";
+        };
+        volumes = [ "n8n_data:/home/node" ];
+        ports = [ "5679:5678" ];
+        autoStart = true;
+        extraOptions = [
+          "--network=slirp4netns:allow_host_loopback=true"
+          "--add-host=n8n.local:10.0.2.2"
+        ];
+      };
+    };
+  };
   services.nginx = {
+    #   virtualHosts."test.${config.globals.domain_name}" = {
+    #     enableACME = true;
+    #     forceSSL = true;
+    #     locations."/" = {
+    #       proxyPass = "http://localhost:5678";
+    #       proxyWebsockets = true;
+    #     };
+    #   };
+
     virtualHosts."n8n.${config.globals.domain_name}" = {
       enableACME = true;
       forceSSL = true;
       locations."/" = {
-        proxyPass = "http://localhost:5678";
+        proxyPass = "http://localhost:5679";
         proxyWebsockets = true;
       };
     };
