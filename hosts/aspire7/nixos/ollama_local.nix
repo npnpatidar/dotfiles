a21af86baf9994de62d844426f58081c624a4e15
npnpatidar <7de6dkm1@duck.com>
2024-08-08 22:57:26 +0530

    ollama folder creation before service start

--- hosts/aspire7/nixos/ollama_local.nix
+++ hosts/aspire7/nixos/ollama_local.nix
@@ -1,21 +1,32 @@
-{ config, ... }: {
-  # services.ollama = {
-  #   enable = true;
-  #   acceleration = "cuda";
-  #   host = "0.0.0.0";
-  # };
+{ config, pkgs, lib, ... }:
 
+let
+  rootPath = "/data/ollama/";
+  openWebUiPath = "${rootPath}/open-webui";
+  ollamaPath = "${rootPath}/ollama";
+in
+{
+  systemd.services."createOllamaDirectory" = {
+    script = ''
+      mkdir -p ${openWebUiPath}
+      mkdir -p ${ollamaPath}
+    '';
+    wantedBy = [ "multi-user.target" ];
+    before = [ "docker-open-webui.service" "docker-ollama.service" ];
+    serviceConfig.Type = "oneshot";
+  };
   virtualisation = {
     oci-containers = {
       containers = {
         open-webui = {
+          hostname = "open-webui";
           image = "ghcr.io/open-webui/open-webui:main";
           autoStart = true;
           ports = [
             "127.0.0.1:8080:8080"
           ];
           volumes = [
-            "/data/ollama/open-webui:/app/backend/data"
+            "${openWebUiPath}:/app/backend/data"
           ];
           environment = {
             OLLAMA_BASE_URL = "http://127.0.0.1:11434";
@@ -23,20 +34,24 @@
           };
           environmentFiles = [ config.age.secrets.open_webui_environment_file.path ];
           extraOptions = [
+            # "--pull=newer"
             "--network=host"
             "--add-host=host.containers.internal:host-gateway"
           ];
         };
 
         ollama = {
+          hostname = "ollama";
           image = "ollama/ollama";
           autoStart = true;
           ports = [ "11434:11434" ];
-          volumes = [ "/data/ollama/ollama:/root/.ollama" ];
-          extraOptions = [ "--gpus=all" ];
+          volumes = [ "${ollamaPath}:/root/.ollama" ];
+          extraOptions = [
+            "--gpus=all"
+            # "--pull=newer"
+          ];
         };
       };
     };
   };
-
 }
