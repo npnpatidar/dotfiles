4cf6232956f05caf060bd65b309eaed0c9417b5b
npnpatidar <7de6dkm1@duck.com>
2024-08-09 06:38:20 +0530

    changedetection directory added

--- hosts/oracle/nixos/changedetection-server.nix
+++ hosts/oracle/nixos/changedetection-server.nix
@@ -1,9 +1,21 @@
 { config, lib, pkgs, ... }:
 let
-  datastorePath = "/var/lib/changedetection-io";
+  datastorePath = "/var/lib/changedetection-io/changedetection-io";
+  webdriverDatapath = "/var/lib/changedetection-io/webdriver";
   baseURL = "change.${config.globals.domain_name}";
 in
 {
+  systemd.services."createChangeDetectionDirectory" = {
+    script = ''
+      mkdir -p ${datastorePath}
+      mkdir -p ${webdriverDatapath}
+    '';
+    wantedBy = [ "multi-user.target" ];
+    before = [ "podman-changedetection-io.service" ];
+    serviceConfig.Type = "oneshot";
+  };
+
+
   services.nginx.virtualHosts."${baseURL}" = {
     forceSSL = true;
     enableACME = true;
@@ -25,7 +37,7 @@ in
           HIDE_REFERER = "false";
           BASE_URL = "${baseURL}";
           USE_X_SETTINGS = "1";
-          # WEBDRIVER_URL = "http://changedetection-io.local:4444/wd/hub";
+          WEBDRIVER_URL = "http://changedetection-io.local:4444/wd/hub";
           PLAYWRIGHT_DRIVER_URL = "ws://changedetection-io.local:4455/?stealth=1&--disable-web-security=true";
         };
         volumes = [ "${datastorePath}:/config" ];
@@ -36,23 +48,23 @@ in
           "--add-host=changedetection-io.local:10.0.2.2"
         ];
       };
-      # changedetection-io-webdriver = {
-      #   image = "selenium/standalone-chromium";
-      #   autoStart = true;
-      #   environment = {
-      #     VNC_NO_PASSWORD = "1";
-      #     SCREEN_WIDTH = "1920";
-      #     SCREEN_HEIGHT = "1080";
-      #     SCREEN_DEPTH = "24";
-      #   };
-      #   ports = [
-      #     "127.0.0.1:4444:4444"
-      #   ];
-      #   volumes = [
-      #     "/dev/shm:/dev/shm"
-      #   ];
-      #   };
-      #
+      changedetection-io-webdriver = {
+        image = "selenium/standalone-chromium";
+        autoStart = true;
+        environment = {
+          VNC_NO_PASSWORD = "1";
+          SCREEN_WIDTH = "1920";
+          SCREEN_HEIGHT = "1080";
+          SCREEN_DEPTH = "24";
+        };
+        ports = [
+          "127.0.0.1:4444:4444"
+        ];
+        volumes = [
+          "${webdriverDatapath}:/dev/shm"
+        ];
+      };
+
       changedetection-io-playwright = {
         image = "ghcr.io/browserless/chromium";
         autoStart = true;
